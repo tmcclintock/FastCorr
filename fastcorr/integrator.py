4c5bfb3ecebbfff2783aba43ec2a76000d83072c
Tom McClintock <tmcclintock89@gmail.com>
2019-03-03 23:08:04 -0800

    Changed everything from bquad to fastcorr.

--- fastcorr/integrator.py
+++ fastcorr/integrator.py
@@ -2,19 +2,19 @@ import numpy as np
 import scipy.interpolate as interp
 import cffi, glob, os
 
-bquad_dir = os.path.dirname(__file__)
-include_dir = os.path.join(bquad_dir,'include')
-lib_file = os.path.join(bquad_dir,'_bquad.so')
+fastcorr_dir = os.path.dirname(__file__)
+include_dir = os.path.join(fastcorr_dir,'include')
+lib_file = os.path.join(fastcorr_dir,'_fastcorr.so')
 # Some installation (e.g. Travis with python 3.x)
-# name this e.g. _bquad.cpython-34m.so,
+# name this e.g. _fastcorr.cpython-34m.so,
 # so if the normal name doesn't exist, look for something else.
 if not os.path.exists(lib_file):
-    alt_files = glob.glob(os.path.join(os.path.dirname(__file__),'_bquad*.so'))
+    alt_files = glob.glob(os.path.join(os.path.dirname(__file__),'_fastcorr*.so'))
     if len(alt_files) == 0:
-        raise IOError("No file '_bquad.so' found in %s"%bquad_dir)
+        raise IOError("No file '_fastcorr.so' found in %s"%fastcorr_dir)
     if len(alt_files) > 1:
-        raise IOError("Multiple files '_bquad*.so' "+\
-                      "found in %s: %s"%(bquad_dir,alt_files))
+        raise IOError("Multiple files '_fastcorr*.so' "+\
+                      "found in %s: %s"%(fastcorr_dir,alt_files))
     lib_file = alt_files[0]
 
 _ffi = cffi.FFI()
--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-
 import sys, os, glob
 import setuptools
 from setuptools import setup, Extension
 import subprocess
 
-os.system('ln -h -s ../include bquad/include')
+os.system('ln -h -s ../include fastcorr/include')
 
 sources = glob.glob(os.path.join('src','*.c'))
 headers = glob.glob(os.path.join('include','*.h'))
@@ -17,34 +16,34 @@ except OSError:
 #Append FFTW flag for the linker
 lflags.append('-lfftw3')
 
-ext=Extension("bquad._bquad",
+ext=Extension("fastcorr._fastcorr",
               sources,
               depends=headers,
               include_dirs=['include'],
               extra_compile_args=[os.path.expandvars(flag) for flag in cflags],
               extra_link_args=[os.path.expandvars(flag) for flag in lflags])
 
-dist = setup(name="bquad",
+dist = setup(name="fastcorr",
              author="Tom McClintock",
              author_email="mcclintock@bnl.gov",
              description="Modules for modeling galaxy clusters and related systematics.",
              license="MIT License",
              url="https://github.com/tmcclintock/Bessel_Quadrature_Integrator",
-             packages=['bquad'],
-             package_data={'bquad' : headers },
+             packages=['fastcorr'],
+             package_data={'fastcorr' : headers },
              ext_modules=[ext],
              install_requires=['cffi','numpy'],
              setup_requires=['pytest_runner'],
              tests_require=['pytest']
 )
 
-#setup.py doesn't put the .so file in the bquad directory, 
+#setup.py doesn't put the .so file in the fastcorr directory, 
 #so this bit makes it possible to
-#import bquad from the root directory.  
+#import fastcorr from the root directory.  
 #Not really advisable, but everyone does it at some
 #point, so might as well facilitate it.
-build_lib = glob.glob(os.path.join('build','*','bquad','_bquad*.so'))
+build_lib = glob.glob(os.path.join('build','*','fastcorr','_fastcorr*.so'))
 if len(build_lib) >= 1:
-    lib = os.path.join('bquad','_bquad.so')
+    lib = os.path.join('fastcorr','_fastcorr.so')
     if os.path.lexists(lib): os.unlink(lib)
     os.link(build_lib[0], lib)
